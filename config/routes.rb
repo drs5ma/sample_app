a5b81937754d0d85071ca3d8966156b6eacfd051
Daniel Saha <drs5ma@virginia.edu>
2016-05-31 12:14:13 -0400

    Add password resets and email configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   #get 'users/new'
@@ -26,6 +30,8 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
