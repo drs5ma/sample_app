1c61db4276aa636ce5e7e9a0c30f7341aa14abf1
Daniel Saha <drs5ma@virginia.edu>
2016-06-01 10:15:41 -0400

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
-
-
-User.create!(name:  "Daniel Saha",
-             email: "drs5ma@virgnia.edu",
-             password:              "password",
-             password_confirmation: "password",
+# Users
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
              admin:     true,
              activated: true,
              activated_at: Time.zone.now)
@@ -21,7 +11,7 @@ User.create!(name:  "Daniel Saha",
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name:  name,
+  User.create!(name: name,
               email: email,
               password:              password,
               password_confirmation: password,
@@ -29,11 +19,17 @@ User.create!(name:  "Daniel Saha",
               activated_at: Time.zone.now)
 end
 
-
+# Microposts
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
 
-
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
